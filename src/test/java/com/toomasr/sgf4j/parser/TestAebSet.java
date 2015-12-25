8c57040fd5a215d4998970423bfb7ce1e2da36c4
Toomas Romer <toomas@zeroturnaround.com>
2015-12-25 23:35:54 +0200

    Lots of games added to automatic tests
    I downloaded a public domain game collection from http://homepages.cwi.nl/~aeb/go/games/index.html
    
    After running through all of the games I also added bunch of properties to be parsed!

---
+++ src/test/java/com/toomasr/sgf4j/parser/TestAebSet.java
@@ -0,0 +1,40 @@
+package com.toomasr.sgf4j.parser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.zeroturnaround.zip.ZipEntryCallback;
+import org.zeroturnaround.zip.ZipUtil;
+
+import com.toomasr.sgf4j.Sgf;
+import com.toomasr.sgf4j.SgfParseException;
+
+public class TestAebSet {
+  @Test
+  public void testAllGamesFromArchive() {
+    Path path = Paths.get("src/test/resources/games-aeb-cwi-nl.zip");
+    ZipUtil.iterate(path.toFile(), new ZipEntryCallback() {
+
+      @Override
+      public void process(InputStream in, ZipEntry zipEntry) throws IOException {
+        if (zipEntry.toString().endsWith("sgf")) {
+
+          try {
+            Sgf.createFromInputStream(in);
+          }
+          catch (SgfParseException e) {
+            System.out.println("Problem with " + zipEntry.getName());
+            e.printStackTrace();
+            Assert.fail();
+          }
+        }
+      }
+    });
+    Assert.assertTrue(true);
+  }
+}
