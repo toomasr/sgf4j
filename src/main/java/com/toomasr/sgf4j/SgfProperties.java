036ad87730e6e431437a2ba03d9aa6c32364896f
Toomas Romer <toomasr@gmail.com>
2017-05-07 16:47:16 +0300

    Some constants for easier SGF field extraction

--- src/main/java/com/toomasr/sgf4j/SgfProperties.java
+++ src/main/java/com/toomasr/sgf4j/SgfProperties.java
@@ -2,5 +2,8 @@ package com.toomasr.sgf4j;
 
 public class SgfProperties {
   public static final String WHITE_PLAYER_NAME = "PW";
+  public static final String WHITE_PLAYER_RATING = "WR";
+
   public static final String BLACK_PLAYER_NAME = "PB";
+  public static final String BLACK_PLAYER_RATING = "BR";
 }
