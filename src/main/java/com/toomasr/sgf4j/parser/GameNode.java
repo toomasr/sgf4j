3e0364e688a2deb384a832429d9e46572f009edb
Toomas Romer <toomasr@gmail.com>
2019-02-21 11:02:01 +0200

    Only reorg of methods. No functional changes.

--- src/main/java/com/toomasr/sgf4j/parser/GameNode.java
+++ src/main/java/com/toomasr/sgf4j/parser/GameNode.java
@@ -27,12 +27,63 @@ public class GameNode implements Comparable<GameNode>, Cloneable {
   private int id;
 
   /**
-   * Constructs a new node with the argument as the parent.
+   * Constructs a new node with the argument as the parent node. Besides a
+   * parent node each node also has possibly a previous and next node. Branching
+   * is achieved by also having children nodes. See the following for a short
+   * overview.
+   *
+   * getNextNode is the node next on the same line of play. If this is null
+   * then the line does not have any more moves.
+   *
+   * getPrevNode is the previous node on the same line of play. If this is null
+   * then this line does not have any previous moves.
+   *
+   * If node hasChildren() is true then this node has child nodes and not just
+   * a nextNode. In that case the getNextNode will be part of the getChildren().
    *
    * @param parentNode node to be the parent of the just created node.
    */
   public GameNode(GameNode parentNode) {
     this.parentNode = parentNode;
+  }
+
+  public void addChild(GameNode node) {
+    if (nextNode == null) {
+      nextNode = node;
+      nextNode.setVisualDepth(0);
+      node.setPrevNode(this);
+      return;
+    }
+
+    if (children.contains(node)) {
+      throw new RuntimeException("Node '" + node + "' already exists for " + this);
+    }
+
+    children.add(node);
+  }
+
+  public void setNextNode(GameNode nextNode) {
+    this.nextNode = nextNode;
+  }
+
+  public GameNode getNextNode() {
+    return nextNode;
+  }
+
+  public void setPrevNode(GameNode node) {
+    this.prevNode = node;
+  }
+
+  public GameNode getPrevNode() {
+    return prevNode;
+  }
+
+  public GameNode getParentNode() {
+    return parentNode;
+  }
+
+  public void setParentNode(GameNode node) {
+    parentNode = node;
   }
 
   public void addProperty(String key, String value) {
@@ -104,39 +155,12 @@ public class GameNode implements Comparable<GameNode>, Cloneable {
     return StoneState.BLACK;
   }
 
-  public void addChild(GameNode node) {
-    if (nextNode == null) {
-      nextNode = node;
-      nextNode.setVisualDepth(0);
-      node.setPrevNode(this);
-      return;
-    }
-
-    if (children.contains(node)) {
-      throw new RuntimeException("Node '" + node + "' already exists for " + this);
-    }
-
-    children.add(node);
-  }
-
   public boolean hasChildren() {
     return children.size() > 0;
   }
 
   public Set<GameNode> getChildren() {
     return children;
-  }
-
-  public GameNode getParentNode() {
-    return parentNode;
-  }
-
-  public void setParentNode(GameNode node) {
-    parentNode = node;
-  }
-
-  public String toString() {
-    return "Props: keys=" + properties.keySet().toString() + " all=" + properties.toString() + " moveNo: " + moveNo + " children: " + children.size() + " vdepth: " + visualDepth+ " parentNode: "+getParentNode().hashCode();
   }
 
   public void setMoveNo(int i) {
@@ -156,55 +180,6 @@ public class GameNode implements Comparable<GameNode>, Cloneable {
   @Override
   protected Object clone() throws CloneNotSupportedException {
     return super.clone();
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((children == null) ? 0 : children.hashCode());
-    result = prime * result + moveNo;
-    result = prime * result + id;
-    result = prime * result + ((parentNode == null) ? 0 : parentNode.properties.hashCode());
-    result = prime * result + ((properties == null) ? 0 : properties.hashCode());
-    result = prime * result + visualDepth;
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    GameNode other = (GameNode) obj;
-    if (children == null) {
-      if (other.children != null)
-        return false;
-    }
-    else if (!children.equals(other.children))
-      return false;
-    if (moveNo != other.moveNo)
-      return false;
-    if (parentNode == null) {
-      if (other.parentNode != null)
-        return false;
-    }
-    else if (!parentNode.equals(other.parentNode))
-      return false;
-    if (properties == null) {
-      if (other.properties != null)
-        return false;
-    }
-    else if (!properties.equals(other.properties))
-      return false;
-    if (visualDepth != other.visualDepth)
-      return false;
-    if (id != other.id)
-      return false;
-    return true;
   }
 
   /**
@@ -296,22 +271,6 @@ public class GameNode implements Comparable<GameNode>, Cloneable {
     return visualDepth;
   }
 
-  public GameNode getNextNode() {
-    return nextNode;
-  }
-
-  public void setNextNode(GameNode nextNode) {
-    this.nextNode = nextNode;
-  }
-
-  public GameNode getPrevNode() {
-    return prevNode;
-  }
-
-  public void setPrevNode(GameNode node) {
-    this.prevNode = node;
-  }
-
   public boolean isPass() {
     // tt means a pass and actually an empty [] also
     // but right now not handling that because I don't know
@@ -351,5 +310,58 @@ public class GameNode implements Comparable<GameNode>, Cloneable {
 
   public int getId() {
     return this.id;
+  }
+
+  public String toString() {
+    return "Props: keys=" + properties.keySet().toString() + " all=" + properties.toString() + " moveNo: " + moveNo + " children: " + children.size() + " vdepth: " + visualDepth+ " parentNode: "+getParentNode().hashCode();
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((children == null) ? 0 : children.hashCode());
+    result = prime * result + moveNo;
+    result = prime * result + id;
+    result = prime * result + ((parentNode == null) ? 0 : parentNode.properties.hashCode());
+    result = prime * result + ((properties == null) ? 0 : properties.hashCode());
+    result = prime * result + visualDepth;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    GameNode other = (GameNode) obj;
+    if (children == null) {
+      if (other.children != null)
+        return false;
+    }
+    else if (!children.equals(other.children))
+      return false;
+    if (moveNo != other.moveNo)
+      return false;
+    if (parentNode == null) {
+      if (other.parentNode != null)
+        return false;
+    }
+    else if (!parentNode.equals(other.parentNode))
+      return false;
+    if (properties == null) {
+      if (other.properties != null)
+        return false;
+    }
+    else if (!properties.equals(other.properties))
+      return false;
+    if (visualDepth != other.visualDepth)
+      return false;
+    if (id != other.id)
+      return false;
+    return true;
   }
 }
